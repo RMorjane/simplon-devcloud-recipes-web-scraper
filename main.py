d70db2526f7ec0d3b56dde1176c38620a2b9e981
Morjane RHELLAB <71873995+RMorjane@users.noreply.github.com>
2021-03-15 01:33:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-from mysql_recipe_scraper import RecipeCategory
+import logging
+from mysql_recipe_scraper import RecipeCategory, MySQLRecipe
+from flask import Flask, render_template, request
+
+logging.basicConfig(filename='logs.log', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+scraping = False
+cnt = MySQLRecipe()
+cnt.set_logger()
+cnt.connect()
+
+@app.route("/", methods=['GET'])
+def display_recipes():
+    list_recipes = []
+    recipe = request.args.get("recipe")
+    if recipe == None:
+        cnt.find_recipes()
+    else:
+        try:
+            recipe_id = int(recipe)
+            cnt.find_recipes({
+                "recipe_id": recipe_id
+            })
+        except:
+            recipe_name = str(recipe)
+            cnt.find_recipes({
+                "recipe_name": recipe_name
+            })
+    list_recipes = cnt.list_recipes
+    return render_template("index.html", recipes=list_recipes)
 
 if __name__ == '__main__':
 
@@ -7,3 +38,5 @@ if __name__ == '__main__':
     recipe_category.read_infos()
     recipe_category.display()
     recipe_category.save_mysql_recipe()
+
+    app.run(host="0.0.0.0", port=3000, debug=True)
